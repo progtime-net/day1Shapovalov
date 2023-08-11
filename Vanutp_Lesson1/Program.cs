30a3434159b901a26dd5c0b65001428dff4b811d
Lev <GitHub16102010@gmail.com>
2023-08-11 18:27:07 +0300

    Add age!

--- Vanutp_Lesson1/Program.cs
+++ Vanutp_Lesson1/Program.cs
@@ -4,9 +4,12 @@
     {
         Console.WriteLine("Hello, World!");
         Console.WriteLine("What is your name?");
-        string name = Console.Read().ToString();
-        Console.Write(" - name");
+        string name = Console.ReadLine();
+        Console.WriteLine(" - name");
 
+        Console.WriteLine("what is your age?");
+        string age = Console.ReadLine();
+        Console.WriteLine($"your age{age}, your age after 10 years: {age + 10}");
         Console.ReadLine();
     }
 }
